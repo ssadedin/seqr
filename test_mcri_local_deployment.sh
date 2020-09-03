ebaa2d39a3bed14d631a437862c3c858bf9c155f
Tommy Li <tommy.li@firefire.co>
2020-09-03 14:26:12 +1000

    feat: Build changes after merging with upstream

---
+++ test_mcri_local_deployment.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+COMPOSE_FILE=mcri_deploy/docker-compose/docker-compose.yml
+COMPOSE_BUILD_FILE=mcri_deploy/docker-compose/docker-compose.build.yml
+COMPOSE_ENV_FILE=mcri_deploy/docker-compose/seqr.sample.env
+
+docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE build
+docker-compose -f $COMPOSE_FILE --env-file=$COMPOSE_ENV_FILE up -d seqr
+docker-compose -f $COMPOSE_FILE --env-file=$COMPOSE_ENV_FILE logs postgres
+docker-compose -f $COMPOSE_FILE --env-file=$COMPOSE_ENV_FILE logs redis
+curl localhost:9200
+sleep 30
+docker-compose -f $COMPOSE_FILE --env-file=$COMPOSE_ENV_FILE logs seqr
+echo -ne 'testpassword\n' docker-compose -f $COMPOSE_FILE --env-file=$COMPOSE_ENV_FILE exec seqr python manage.py createsuperuser --username test --email test@test.com
