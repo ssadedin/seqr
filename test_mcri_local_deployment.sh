776ed151fee821102d1fd7e996720dbc82359762
Tommy Li <tommy.li@firefire.co>
2020-09-04 18:04:41 +1000

    feat: MCRI specific build

--- test_mcri_local_deployment.sh
+++ test_mcri_local_deployment.sh
@@ -2,14 +2,12 @@
 
 set -ex
 
-COMPOSE_FILE=mcri_deploy/docker-compose/docker-compose.yml
-COMPOSE_BUILD_FILE=mcri_deploy/docker-compose/docker-compose.build.yml
-COMPOSE_ENV_FILE=mcri_deploy/docker-compose/seqr.sample.env
+cp "./mcri_deploy/docker-compose/seqr.sample.env" "./.env"
+cp "./mcri_deploy/docker-compose/docker-compose.yml" "./"
 
-docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE up -d seqr
-docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE logs postgres
-docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE logs redis
-
+docker-compose up -d seqr
+docker-compose logs postgres
+docker-compose logs redis
 sleep 30
-docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE logs seqr
-echo -ne 'testpassword\n' docker-compose -f $COMPOSE_FILE -f $COMPOSE_BUILD_FILE --env-file=$COMPOSE_ENV_FILE exec seqr python manage.py createsuperuser --username test --email test@test.com
+docker-compose logs seqr
+echo -ne 'testpassword\n' docker-compose exec seqr python manage.py createsuperuser --username test --email test@test.com
